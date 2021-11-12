8c7c53e80b5bbe6a75ee67a659858b036b0bc080
hugcis <hmj.cisneros@gmail.com>
2021-11-12 17:05:41 +0100

    Update exp

--- experiments/symbol_counting/hard_symbol_counting_rbf.py
+++ experiments/symbol_counting/hard_symbol_counting_rbf.py
@@ -3,5 +3,5 @@ from reservoir_ca.tasks import HardSymbolCounting
 from experiments.helpers import run_task
 
 if __name__ == "__main__":
-    run_task("hard_sym_count_rbd_exp#.pkl", HardSymbolCounting, [10],
+    run_task("hard_sym_count_rbf2_exp#.pkl", HardSymbolCounting, [25],
              opts_extra={"reg_type": RegType.RBFSVM})
