50b44394136f7ca4d3821d54581c7edb2b2d4cf8
hugcis <hmj.cisneros@gmail.com>
2021-11-15 16:53:35 +0100

    New exps

---
+++ experiments/periodic/periodic_one2many.py
@@ -0,0 +1,6 @@
+from reservoir_ca.ca_res import ProjectionType
+from reservoir_ca.tasks import Periodic
+from experiments.helpers import run_task
+
+if __name__ == "__main__":
+    run_task(Periodic, [5], opts_extra={"proj_type": ProjectionType.ONE_TO_MANY})
