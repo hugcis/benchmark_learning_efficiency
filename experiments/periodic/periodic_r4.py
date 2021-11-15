8849182282f085fb26abb16efa4a572813f035cf
hugcis <hmj.cisneros@gmail.com>
2021-11-15 15:44:20 +0100

    Add more experiments

---
+++ experiments/periodic/periodic_r4.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+
+from reservoir_ca.tasks import Periodic
+from reservoir_ca.ca_res import CAReservoir
+from reservoir_ca.experiment import Experiment
+from experiments.helpers import init_exp
+
+if __name__ == "__main__":
+    res, opts = init_exp("periodic_exp#.pkl")
+    opts.r_height = 4
+    for _ in tqdm(range(opts.n_rep)):
+        per = Periodic(5)
+        ca = CAReservoir(0, 2)
+        exp = Experiment(ca, per, opts)
+        for t in opts.rules:
+            ca = CAReservoir(t, 2, redundancy=opts.redundancy)
+            exp.ca = ca
+            exp.fit()
+            res.update(t, exp.eval_test())
+    res.save()
