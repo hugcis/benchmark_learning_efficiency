6b08ec6b5968061341e2c7f2fe71ac37ab55bf34
hugcis <hmj.cisneros@gmail.com>
2021-11-16 18:15:39 +0100

    Update hard symbol experiments

--- experiments/symbol_counting/hard_symbol_counting_rbf.py
+++ experiments/symbol_counting/hard_symbol_counting_rbf.py
@@ -3,4 +3,4 @@ from reservoir_ca.tasks import HardSymbolCounting
 from experiments.helpers import run_task
 
 if __name__ == "__main__":
-    run_task( HardSymbolCounting, [25], opts_extra={"reg_type": RegType.RBFSVM})
+    run_task(HardSymbolCounting, [25], opts_extra={"reg_type": RegType.RBFSVM})
--- experiments/symbol_counting/hard_symbol_counting_rbf_masked.py
+++ experiments/symbol_counting/hard_symbol_counting_rbf_masked.py
@@ -3,6 +3,5 @@ from reservoir_ca.tasks import HardSymbolCounting
 from experiments.helpers import run_task
 
 if __name__ == "__main__":
-    run_task("hard_symbol_counting_rbf_masked_exp#.pkl",
-             HardSymbolCounting, [25],
+    run_task(HardSymbolCounting, [25],
              opts_extra={"reg_type": RegType.RBFSVM, "ignore_mask": False})
