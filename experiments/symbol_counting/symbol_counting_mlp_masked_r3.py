1a3d850f04c100b18b662453b7a2ae3ec47492e3
hugcis <hmj.cisneros@gmail.com>
2021-11-18 11:53:47 +0100

    Larger reservoirs for MLP symbol counting

---
+++ experiments/symbol_counting/symbol_counting_mlp_masked_r3.py
@@ -0,0 +1,8 @@
+from reservoir_ca.experiment import RegType
+from reservoir_ca.tasks import SymbolCounting
+from experiments.helpers import run_task
+
+if __name__ == "__main__":
+    run_task(SymbolCounting, [10], opts_extra={"reg_type": RegType.MLP,
+                                               "ignore_mask": False,
+                                               "r_height": 3})
