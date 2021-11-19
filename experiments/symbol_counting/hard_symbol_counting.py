c2fdabc80680c98a83c3591dd10efbd3939c286e
hugcis <hmj.cisneros@gmail.com>
2021-11-19 12:03:46 +0100

    Fix experiment new format

--- experiments/symbol_counting/hard_symbol_counting.py
+++ experiments/symbol_counting/hard_symbol_counting.py
@@ -2,4 +2,4 @@ from reservoir_ca.tasks import HardSymbolCounting
 from experiments.helpers import run_task
 
 if __name__ == "__main__":
-    run_task("hard_sym_count_exp#.pkl", HardSymbolCounting, [10])
+    run_task(HardSymbolCounting, [25])
