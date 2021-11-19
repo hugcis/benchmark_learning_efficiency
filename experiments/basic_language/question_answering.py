2a0edcabda85efdf765bc0a4ad37ec9f7ccca07b
hugcis <hmj.cisneros@gmail.com>
2021-11-19 16:39:44 +0100

    Remove assumptions in language task

--- experiments/basic_language/question_answering.py
+++ experiments/basic_language/question_answering.py
@@ -1,6 +1,5 @@
 from reservoir_ca.tasks import ElementaryLanguage
-from reservoir_ca.experiment import RegType
 from experiments.helpers import run_task
 
 if __name__ == "__main__":
-    run_task(ElementaryLanguage, [], opts_extra={"reg_type": RegType.RBFSVM})
+    run_task(ElementaryLanguage, [])
