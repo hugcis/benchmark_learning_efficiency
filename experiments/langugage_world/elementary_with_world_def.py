750a1f0408a8de67c0388852b6249b9811a95149
hugcis <hmj.cisneros@gmail.com>
2022-01-11 10:18:03 +0100

    Add an experiment for the new task

---
+++ experiments/langugage_world/elementary_with_world_def.py
@@ -0,0 +1,5 @@
+from reservoir_ca.tasks import ElementaryLanguageWithWorldDef
+from experiments.helpers import run_task
+
+if __name__ == "__main__":
+    run_task(ElementaryLanguageWithWorldDef, [])
